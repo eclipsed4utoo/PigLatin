0d3c9f64314ac8720d030896510764d157f68531
Ryan Alford <ryan@eclipsed4utoo.com>
2013-04-27 10:49:04 -0400

    initial commit

---
+++ src/PigLatin.Phone/PigLatin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text;
+
+namespace PigLatin.Phone
+{
+    public class PigLatin
+    {
+        private static string vowels = "AEIOUaeiuo";
+        private static string specialCharacters = " ~`!@#$%^&*()'\";:/?.>,<";
+
+        public static string ConvertToPigLatin(string sentence)
+        {
+            string[] words = sentence.Split(specialCharacters.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                string newWord = ConvertWordToPigLatin(word);
+
+                sb.Append(newWord + " ");
+            }
+
+            return sb.ToString();
+        }
+
+        private static string ConvertWordToPigLatin(string word)
+        {
+            int vowelIndex = GetIndexOfFirstVowel(word);
+
+            string newWord = word.Substring(vowelIndex) + word.Substring(0, vowelIndex) + "ay";
+
+            return newWord;
+        }
+
+        private static int GetIndexOfFirstVowel(string word)
+        {
+            int index = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (vowels.Contains(word[i].ToString()))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ src/PigLatin/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PigLatin
+{
+    class Program
+    {
+        private static string vowels = "AEIOUaeiuo";
+        private static string specialCharacters = " ~`!@#$%^&*()'\";:/?.>,<";
+
+        static void Main(string[] args)
+        {
+            string text = "hello there my little friend.";
+            
+            string[] words = text.Split(specialCharacters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string word in words)
+            {
+
+                string newWord = ConvertToPigLatin(word);
+
+                sb.Append(newWord + " ");
+            }
+
+            Console.WriteLine(sb.ToString());
+            Console.Read();
+        }
+
+        private static string ConvertToPigLatin(string word)
+        {
+            int vowelIndex = GetIndexOfFirstVowel(word);
+
+            string newWord = word.Substring(vowelIndex) + word.Substring(0, vowelIndex) + "ay";
+
+            return newWord;
+        }
+
+        private static int GetIndexOfFirstVowel(string word)
+        {
+            int index = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (vowels.Contains(word[i]))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+    }
+}
